ad1f1d2fbe5d99e1a913fb1a2f81a61953d432db
mgargas <250497@wp.pl>
2019-11-16 22:02:16 +0100

    implement service charging based on order items

--- src/main/scala/com/cafex/BillCalculator.scala
+++ src/main/scala/com/cafex/BillCalculator.scala
@@ -1,9 +1,20 @@
 package com.cafex
 
-import com.cafex.Model.CafeItem
+import com.cafex.Model.{CafeItem, Food, Hot}
 
 object BillCalculator {
   def calculate(order: List[CafeItem]): BigDecimal = {
-    order.map(_.price).foldLeft(BigDecimal.apply(0))(_ + _)
+    def calculateNoServiceCharge = order.map(_.price).foldLeft(BigDecimal.apply(0))(_ + _)
+
+    if (order.exists {
+      case item: Food if item.temperature == Hot => true
+      case _ => false
+    }) calculateNoServiceCharge.*(1.20).setScale(2, BigDecimal.RoundingMode.UP)
+
+    else if (order.exists {
+      case _: Food => true
+      case _ => false
+    }) calculateNoServiceCharge.*(1.10).setScale(2, BigDecimal.RoundingMode.UP)
+    else calculateNoServiceCharge.setScale(2, BigDecimal.RoundingMode.UP)
   }
 }
--- src/test/scala/com/cafex/CafeXTests.scala
+++ src/test/scala/com/cafex/CafeXTests.scala
@@ -3,10 +3,20 @@ package com.cafex
 import org.scalatest.{BeforeAndAfter, FunSuite}
 
 class CafeXTests extends FunSuite with BeforeAndAfter {
-  val order = List("Cola", "Coffee", "Cheese Sandwich")
 
-  test("system can calculate total bill without tips") {
+  test("system can calculate total bill for only beverages") {
+    val order = List("Cola", "Coffee", "Coffee")
     val parsedOrder = Parser.parseOrder(order)
-    assert(BillCalculator.calculate(parsedOrder) == BigDecimal.apply(3.5))
+    assert(BillCalculator.calculate(parsedOrder) == BigDecimal.apply(2.5))
+  }
+  test("system can calculate total bill with 10% charge when the order contains any cold food") {
+    val order = List("Cola", "Coffee", "Cheese Sandwich")
+    val parsedOrder = Parser.parseOrder(order)
+    assert(BillCalculator.calculate(parsedOrder) == BigDecimal.apply(3.85))
+  }
+  test("system can calculate total bill with 20% charge when the order contains any hot food") {
+    val order = List("Cola", "Coffee", "Steak Sandwich")
+    val parsedOrder = Parser.parseOrder(order)
+    assert(BillCalculator.calculate(parsedOrder) == BigDecimal.apply(7.20))
   }
 }
